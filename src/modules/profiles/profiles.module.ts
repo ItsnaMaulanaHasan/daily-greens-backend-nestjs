5ea5af12af3b35241eb1051d71462f120d64a240
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-09-15 07:26:31 +0700

    feat(profile): add CloudinaryModule to ProfilesModule for image management

--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryModule } from '../../common/cloudinary/cloudinary.module';
 import { AuthModule } from '../auth/auth.module';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CloudinaryModule],
   controllers: [ProfilesController],
   providers: [ProfilesService],
 })
